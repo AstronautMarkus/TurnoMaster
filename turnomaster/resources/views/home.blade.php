159f854791ccdc47beae5402b47b94d75010088e
Marcos Reyes <astronautmarkus@gmail.com>
2025-02-09 17:18:30 -0300

    feat: Add custom CSS for home page styling and layout improvements template

--- turnomaster/resources/views/home.blade.php
+++ turnomaster/resources/views/home.blade.php
@@ -2,6 +2,26 @@
 
 @section('title', 'TurnoMaster - home')
 
+<link rel="stylesheet" href="{{ asset('css/home-blade.css') }}">
+
 @section('content')
-    <h1>Lorem ipsum</h1>
+    <div class="hero">
+        
+            <div class="row justify-content-center align-items-center">
+                <div class="col-md-5">
+                    <div class="intro-text">
+                        <h1>¿Estás listo para optimizar la gestión de horarios de tus empleados?</h1>
+                        <p>TurnoMaster es el software ideal para gestionar las entradas y salidas de tu negocio, 
+                        permitiéndote conectar mejor con tus empleados y optimizar la administración de tu empresa.</p>
+                        <a href="/" class="btn btn-primary">Comenzar ahora</a>
+                    </div>
+                </div>
+                <div class="col-md-5">
+                    <div class="intro-image">
+                        <img src="{{ asset('img/home/example-home.png') }}" alt="TurnoMaster" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        
+    </div>
 @endsection
--- turnomaster/resources/views/layouts/app.blade.php
+++ turnomaster/resources/views/layouts/app.blade.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">    
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app-blade.css') }}">
 </head>
 <body>
     <x-header />
-    <div>
-        @yield('content')
-    </div>
+    @yield('content')
 </body>
 <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
 </html>
