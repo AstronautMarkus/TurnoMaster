25ddc27662d69fbfaa24eac7d974f08226c398cc
Marcos Reyes <astronautmarkus@gmail.com>
2025-02-15 17:09:11 -0300

    feat: Add dashboard view and route for authenticated users

--- turnomaster/routes/web.php
+++ turnomaster/routes/web.php
@@ -10,3 +10,8 @@ Route::get('/login', function () {
     return view('login');
 });
 
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    });
+});
