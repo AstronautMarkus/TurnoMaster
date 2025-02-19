756462cfabd0a053b7b4369665beaaf7dc14c652
Marcos Reyes <astronautmarkus@gmail.com>
2025-02-18 21:21:32 -0300

    feat: Update login form to display error messages in Spanish and retain email input value

--- turnomaster/resources/views/login.blade.php
+++ turnomaster/resources/views/login.blade.php
@@ -10,6 +10,7 @@
                 <div class="card-header">{{ __('Iniciar sesión') }}</div>
 
                 <div class="card-body">
+
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
@@ -17,7 +18,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo electrónico') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" required autocomplete="email" autofocus>
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                             </div>
                         </div>
 
@@ -28,6 +29,16 @@
                                 <input id="password" type="password" class="form-control" name="password" required autocomplete="current-password">
                             </div>
                         </div>
+
+                        @if ($errors->any())
+                        <div>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <p class="text-danger text-center">{{ $error }}</p>
+                                @endforeach
+                            </ul>
+                        </div>
+                        @endif
 
                         <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
@@ -44,11 +55,11 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                                    {{ __('Iniciar sesión') }}
                                 </button>
 
                                 <a class="btn btn-link" href="#">
-                                    {{ __('Forgot Your Password?') }}
+                                    {{ __('¿Olvidaste tu contraseña?') }}
                                 </a>
                             </div>
                         </div>
