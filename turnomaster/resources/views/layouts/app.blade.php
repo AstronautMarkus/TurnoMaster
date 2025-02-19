d70ec77b934b83935ef6ccfa5956c0518ab77660
Marcos Reyes <astronautmarkus@gmail.com>
2025-02-18 22:34:31 -0300

    feat: Replace Blade components with include directives for navbar and footer

--- turnomaster/resources/views/layouts/app.blade.php
+++ turnomaster/resources/views/layouts/app.blade.php
@@ -10,13 +10,13 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 <body class="d-flex flex-column min-vh-100">
-    <x-navbar />
+    @include('components.navbar')
 
     <main class="">
         @yield('content')
     </main>
 
-    <x-footer />
+    @include('components.footer')
 </body>
 
 <script src="{{ asset('js/jquery-3.7.1.min.js') }}"></script>
