f55c0a197115b554a3514b77cc37a2c1310a78a2
Marcos Reyes <astronautmarkus@gmail.com>
2025-02-20 15:10:45 -0300

    feat: added bootstrap modal for confirm logout in logged pannel.

--- turnomaster/resources/views/components/navbar.blade.php
+++ turnomaster/resources/views/components/navbar.blade.php
@@ -26,10 +26,7 @@
                         Hola, {{ ucfirst(Auth::user()->name) }}!
                     </a>
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-                            <button type="submit" class="dropdown-item">Cerrar sesión</button>
-                        </form>
+                        <button type="button" class="dropdown-item" data-toggle="modal" data-target="#logoutModal">Cerrar sesión</button>
                     </div>
                 </li>
             @else
@@ -41,5 +38,29 @@
                 </li>
             @endauth
         </ul>
+    </div>
+</div>
+
+
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="logoutModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="logoutModalLabel">Confirmar Cierre de Sesión</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                ¿Estás seguro de que deseas cerrar sesión?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit" class="btn btn-primary">Cerrar sesión</button>
+                </form>
+            </div>
+        </div>
     </div>
 </div>
