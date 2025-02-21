44300405e94efe7d46161d6f35754eb5f669d8d7
Marcos Reyes <astronautmarkus@gmail.com>
2025-02-20 21:29:13 -0300

    feat: update about us page styles and improve card structure

--- turnomaster/resources/views/home/about-us.blade.php
+++ turnomaster/resources/views/home/about-us.blade.php
@@ -5,17 +5,17 @@
 <link rel="stylesheet" href="{{ asset('css/about-us/about-us.css') }}">
 
 @section('content')
-<div class="container mt-5">
+<div class="container about_us-container mt-5">
     <h2 class="text-center mt-5">Creadores del Proyecto</h2>
     <div class="row mt-4">
         <div class="col-md-6">
-            <div class="card">
+            <div class="card about_us-card">
                 <img src="https://github.com/AstronautMarkus.png" class="card-img-top" alt="AstronautMarkus">
                 <div class="card-body">
-                    <h5 class="card-title">AstronautMarkus (Marcos Reyes)</h5>
+                    <h5 class="card-title about_us-card-title">AstronautMarkus (Marcos Reyes)</h5>
                     <p class="card-text">Desarrollador principal y arquitecto del sistema. Experto en backend y gestión de bases de datos.</p>
-                    <a href="https://github.com/AstronautMarkus" class="btn btn-primary">Ver Perfil</a>
-                    <div class="social-links">
+                    <a href="https://github.com/AstronautMarkus" class="btn btn-secondary">Ver Perfil</a>
+                    <div class="social-links about_us-social-links">
                         <a href="https://linkedin.com/in/AstronautMarkus" target="_blank"><i class="bi bi-linkedin"></i></a>
                         <a href="https://twitter.com/AstronautMarkus" target="_blank"><i class="bi bi-twitter"></i></a>
                     </div>
@@ -26,10 +26,10 @@
             <div class="card">
                 <img src="https://github.com/Ki4ra1109.png" class="card-img-top" alt="Ki4ra1109">
                 <div class="card-body">
-                    <h5 class="card-title">Ki4ra1109 (Kiara Rubio)</h5>
+                    <h5 class="card-title about_us-card-title">Ki4ra1109 (Kiara Rubio)</h5>
                     <p class="card-text">Especialista en frontend y diseño de interfaces. Responsable de la experiencia de usuario y diseño visual.</p>
-                    <a href="https://github.com/Ki4ra1109" class="btn btn-primary">Ver Perfil</a>
-                    <div class="social-links">
+                    <a href="https://github.com/Ki4ra1109" class="btn btn-secondary">Ver Perfil</a>
+                    <div class="social-links about_us-social-links">
                         <a href="https://linkedin.com/in/Ki4ra1109" target="_blank"><i class="bi bi-linkedin"></i></a>
                         <a href="https://twitter.com/Ki4ra1109" target="_blank"><i class="bi bi-twitter"></i></a>
                     </div>
