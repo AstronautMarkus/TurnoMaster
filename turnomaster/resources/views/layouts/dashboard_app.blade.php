804c4d76404f80fc6ecccc2546fe9b37bf4d7f8d
Marcos Reyes <astronautmarkus@gmail.com>
2025-02-21 19:58:45 -0300

    feat: create dashboard layout and update home view to use new layout

--- turnomaster/resources/views/layouts/dashboard_app.blade.php
+++ turnomaster/resources/views/layouts/dashboard_app.blade.php
@@ -5,18 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/app-blade.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/dashboard/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/dashboard/dashboard-app-blade.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 <body class="d-flex flex-column min-vh-100">
-    @include('components.navbar')
-
-    <main class="">
+    
+    <main >
         @yield('content')
     </main>
 
-    @include('components.footer')
 </body>
 
 <script src="{{ asset('js/jquery-3.7.1.min.js') }}"></script>
