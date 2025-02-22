c63a0ba40cf93f0c3259d4618635bd4c0d8db609
Marcos Reyes <astronautmarkus@gmail.com>
2025-02-22 20:01:09 -0300

    refactor: create dashboard home view and update route to point to new view

--- turnomaster/routes/web.php
+++ turnomaster/routes/web.php
@@ -27,7 +27,7 @@ Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', function () {
-        return view('dashboard.home');
+        return view('dashboard.dashboard_home');
     });
 });
 
