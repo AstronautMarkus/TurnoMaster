3a487502573baa6f31374bcfeb5ca2221ad8df8b
Marcos Reyes <astronautmarkus@gmail.com>
2025-02-21 22:43:48 -0300

    feat: add navbar and sidebar stylesheets and link them in dashboard components

--- turnomaster/resources/views/components/dashboard_navbar.blade.php
+++ turnomaster/resources/views/components/dashboard_navbar.blade.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="{{ asset('css/dashboard/components/navbar/navbar.css') }}">
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">Navbar</a>
