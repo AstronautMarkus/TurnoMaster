9021c99e4e514584ba937aa4b6773f224142983d
Marcos Reyes <astronautmarkus@gmail.com>
2025-02-21 22:55:03 -0300

    feat: enhance navbar layout by aligning items to the right and updating greeting text

--- turnomaster/resources/views/components/dashboard_navbar.blade.php
+++ turnomaster/resources/views/components/dashboard_navbar.blade.php
@@ -2,28 +2,13 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-primary text-white w-100 navbar-custom">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
-                </li>
-            </ul>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
             <span class="navbar-text me-3">
-                Hola, {usuario}
+                ¡Hola, {usuario}!
             </span>
             <div class="dropdown">
                 <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown button
+                    <span class="navbar-toggler-icon"></span>
                 </button>
                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                     <li><a class="dropdown-item" href="#">Action</a></li>
