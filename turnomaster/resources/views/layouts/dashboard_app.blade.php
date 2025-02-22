be8d2b175cb1e2fb93f1ce01ba745d41b3d0c83a
Marcos Reyes <astronautmarkus@gmail.com>
2025-02-21 21:10:34 -0300

    feat: add dashboard layout with navbar and sidebar components

--- turnomaster/resources/views/layouts/dashboard_app.blade.php
+++ turnomaster/resources/views/layouts/dashboard_app.blade.php
@@ -10,7 +10,9 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 <body class="d-flex flex-column min-vh-100">
-    
+    @include('components.dashboard_navbar')
+    @include('components.dashboard_sidebar')
+
     <main >
         @yield('content')
     </main>
