25c241db78c322da1cc88b59337816c6440b9bc1
Marcos Reyes <astronautmarkus@gmail.com>
2025-02-23 02:33:19 -0300

    style: add navbar styles and update font usage for improved aesthetics and responsiveness

--- turnomaster/resources/views/components/navbar.blade.php
+++ turnomaster/resources/views/components/navbar.blade.php
@@ -1,11 +1,7 @@
-<link rel="stylesheet" href="{{ asset('css/index/components/navbar/navbar-blade.css') }}">
+<link rel="stylesheet" href="{{ asset('css/index/components/navbar/navbar.css') }}">
 
-<div class="navbar navbar-expand-lg navbar-color  font-weight-bold">
-    @auth
-        <a class="navbar-brand font-weight-bold" href="/dashboard">TurnoMaster</a>
-    @else
-        <a class="navbar-brand navbar-text font-weight-bold" href="/">TurnoMaster</a>
-    @endauth
+<div class="navbar navbar-expand-lg navbar-color">
+    <a class="navbar-brand navbar-text font-weight-bold" href="/dashboard">TurnoMaster</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -22,46 +18,12 @@
             </li>
         </ul>
         <ul class="navbar-nav">
-            @auth
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle navbar-text font-weight-normal" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Hola, {{ ucfirst(Auth::user()->name) }}!
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <button type="button" class="dropdown-item navbar-text font-weight-normal" data-toggle="modal" data-target="#logoutModal">Cerrar sesión</button>
-                    </div>
-                </li>
-            @else
                 <li class="nav-item">
                     <a class="nav-link navbar-text font-weight-normal" href="/register">Registrarse</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link navbar-text font-weight-normal btn-custom nav-btn-center" href="/login">Iniciar sesión</a>
                 </li>
-            @endauth
         </ul>
-    </div>
-</div>
-
-<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="logoutModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title navbar-text font-weight-bold" id="logoutModalLabel">Confirmar Cierre de Sesión</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body navbar-text">
-                ¿Estás seguro de que deseas cerrar sesión?
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-                    <button type="submit" class="btn btn-primary">Cerrar sesión</button>
-                </form>
-            </div>
-        </div>
     </div>
 </div>
