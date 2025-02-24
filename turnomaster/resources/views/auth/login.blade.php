e31bf1060f82866decdc152790b7c01afb947732
Marcos Reyes <astronautmarkus@gmail.com>
2025-02-24 20:25:35 -0300

    feat: enhance login and registration headers with logo and improved layout

--- turnomaster/resources/views/auth/login.blade.php
+++ turnomaster/resources/views/auth/login.blade.php
@@ -7,9 +7,12 @@
 @section('content')
 <div class="d-flex justify-content-center align-items-center vh-100">
     <div class="login-container">
-        <div class="login-header">
-            <h2>{{ __('Iniciar sesión') }}</h2>
-            <p>Accede a tu cuenta para continuar</p>
+        <div class="login-header d-flex align-items-center">
+            <img src="{{ asset('img/logo/TurnoMaster_lines.svg') }}" alt="Login Image" class="login-image">
+            <div class="login-title">
+                <h2>{{ __('Iniciar sesión') }}</h2>
+                <p>Accede a tu cuenta para continuar</p>
+            </div>
         </div>
         <form method="POST" action="{{ route('login') }}">
             @csrf
