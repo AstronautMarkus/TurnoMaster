c3bcb5952d8900a8ff67691dfaa05c9adb82c299
Marcos Reyes <astronautmarkus@gmail.com>
2025-02-24 22:10:09 -0300

    feat: add email handler styles and enhance verification error page layout

--- turnomaster/resources/views/handlers/email_handler.blade.php
+++ turnomaster/resources/views/handlers/email_handler.blade.php
@@ -1,13 +1,25 @@
 @extends('layouts.auth_app')
 
+<link rel="stylesheet" href="{{ asset('css/auth/email_handler/email_handler.css') }}">
+
 @section('title', 'TurnoMaster - Error de Verificación')
 
 @section('content')
-<div class="verification-error-container">
-    <h2>{{ __('Error de Verificación') }}</h2>
-    @if (session('error'))
-        <p>{{ session('error') }}</p>
-    @endif
-    <p>{{ __('Por favor, solicite un nuevo código de verificación.') }}</p>
+<a href="{{ url('/') }}" class="home-button">
+    <i class="bi bi-house-door-fill"></i>{{ __('Inicio') }}
+</a>
+<div>
+    <div class="login-container">
+        <div class="login-header d-flex align-items-center">
+            <img src="{{ asset('img/logo/TurnoMaster_lines.svg') }}" alt="Error Image" class="login-image">
+            <div class="login-title">
+                <h2>{{ __('Error de Verificación') }}</h2>
+                <p>{{ __('Por favor, solicite un nuevo código de verificación.') }}</p>
+            </div>
+        </div>
+        @if (session('error'))
+            <p class="text-danger">{{ session('error') }}</p>
+        @endif
+    </div>
 </div>
 @endsection
