9593fb2b7508bb17cff850f7281d6c5bf71d71ba
Marcos Reyes <astronautmarkus@gmail.com>
2025-02-27 00:10:43 -0300

    feat: implement expandable menu in sidebar for improved navigation

--- turnomaster/resources/views/components/dashboard_sidebar.blade.php
+++ turnomaster/resources/views/components/dashboard_sidebar.blade.php
@@ -1,36 +1,60 @@
 <link rel="stylesheet" href="{{ asset('css/dashboard/components/sidebar/sidebar.css') }}">
 
-<div class="d-flex flex-column flex-shrink-0 p-3 text-white vh-100 sidebar" style="width: 280px;">
-    
-<a class="navbar-brand navbar-text font-weight-bold" href="/">
-        <img src="{{ asset('img/logo/TurnoMaster.svg') }}" alt="TurnoMaster Logo" class="navbar-logo" id="navbar-logo">
-    </a>
-    <hr>
-    <ul class="nav nav-pills flex-column mb-auto separator">
-        <li class="nav-item">
-            <a href="/" class="nav-link" aria-current="page">
-                <i class="fas fa-home me-2"></i>
-                Opción interesante 1
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="/" class="nav-link">
-                <i class="fas fa-user me-2"></i>
-                Opción interesante 2
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="/" class="nav-link">
-                <i class="fas fa-cog me-2"></i>
-                Opción interesante 3
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="/" class="nav-link">
-                <i class="fas fa-sign-out-alt me-2"></i>
-                Opción interesante 4
-            </a>
-        </li>
-    </ul>
-    <hr>
+<div class="d-flex flex-column flex-shrink-0 text-white vh-100 sidebar" style="width: 280px;">
+    <div class="sidebar-header p-3">
+        <a class="navbar-brand navbar-text font-weight-bold" href="/">
+            <img src="{{ asset('img/logo/TurnoMaster.svg') }}" alt="TurnoMaster Logo" class="navbar-logo" id="navbar-logo">
+        </a>
+        <hr>
+    </div>
+    <div class="sidebar-content p-3" style="overflow-y: auto;">
+        <ul class="nav nav-pills flex-column mb-auto separator">
+            <li class="nav-item">
+                <a href="/" class="nav-link" aria-current="page">
+                    <i class="fas fa-home me-2"></i>
+                    Opción interesante 1
+                </a>
+            </li>
+            <li class="nav-item expandable-menu">
+                <a href="#" class="nav-link">
+                    <i class="fas fa-folder me-2"></i>
+                    Menú Expandible
+                </a>
+                <ul class="expandable-menu-content">
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">
+                            Sub Opción 1
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">
+                            Sub Opción 2
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a href="/" class="nav-link">
+                    <i class="fas fa-user me-2"></i>
+                    Opción interesante 2
+                </a>
+            </li>
+            
+            </li>
+        </ul>
+        <hr>
+    </div>
 </div>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const expandableMenus = document.querySelectorAll('.expandable-menu');
+
+        expandableMenus.forEach(menu => {
+            menu.addEventListener('click', function() {
+                const expandableMenuContent = menu.querySelector('.expandable-menu-content');
+                expandableMenuContent.classList.toggle('show');
+            });
+        });
+    });
+</script>
