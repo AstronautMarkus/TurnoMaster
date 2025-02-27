a7a8c66db410a1f7a98faa547ac2ea126ca6afff
Ki4ra1109 <ki.rubio@duocuc.cl>
2025-02-27 15:46:10 -0300

    style: add new card_features class for enhanced layout and styling of feature cards

--- turnomaster/resources/views/home/features.blade.php
+++ turnomaster/resources/views/home/features.blade.php
@@ -7,44 +7,44 @@
     <h1 class="text-center" style="font-family: 'Poppins', sans-serif;">Características de TurnoMaster</h1>
     <div class="row mt-4" style="font-family: 'Roboto', sans-serif;">
         <div class="col-md-4 d-flex align-items-stretch">
-            <div class="card text-center shadow-sm">
-                <div class="card-body">
-                    <h5 class="card-title">Gestión de Turnos</h5>
-                    <p class="card-text">Organiza y administra los turnos de tus empleados de manera eficiente. Crea, edita y elimina turnos con facilidad.</p>
+            <div class="card_features">
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title about_us-card-title">Gestión de Turnos</h5>
+                    <p class="card-text about_us-card-text">Organiza y administra los turnos de tus empleados de manera eficiente. Crea, edita y elimina turnos con facilidad.</p>
                 </div>
             </div>
         </div>
         <div class="col-md-4 d-flex align-items-stretch">
-            <div class="card text-center shadow-sm">
-                <div class="card-body">
-                    <h5 class="card-title">Notificaciones en Tiempo Real</h5>
-                    <p class="card-text">Recibe notificaciones instantáneas sobre cambios en los turnos, solicitudes de permisos y más, directamente en tu correo electrónico.</p>
+            <div class="card_features">
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title about_us-card-title">Notificaciones en Tiempo Real</h5>
+                    <p class="card-text about_us-card-text">Recibe notificaciones instantáneas sobre cambios en los turnos, solicitudes de permisos y más, directamente en tu correo electrónico.</p>
                 </div>
             </div>
         </div>
         <div class="col-md-4 d-flex align-items-stretch">
-            <div class="card text-center shadow-sm">
-                <div class="card-body">
-                    <h5 class="card-title">Reportes Detallados</h5>
-                    <p class="card-text">Genera reportes detallados sobre la asistencia, horas trabajadas y rendimiento de tus empleados para una mejor toma de decisiones.</p>
+            <div class="card_features">
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title about_us-card-title">Reportes Detallados</h5>
+                    <p class="card-text about_us-card-text">Genera reportes detallados sobre la asistencia, horas trabajadas y rendimiento de tus empleados para una mejor toma de decisiones.</p>
                 </div>
             </div>
         </div>
     </div>
     <div class="row mt-4 justify-content-center" style="font-family: 'Roboto', sans-serif;">
         <div class="col-md-4 d-flex align-items-stretch">
-            <div class="card text-center shadow-sm">
-                <div class="card-body">
-                    <h5 class="card-title">Panel de Control</h5>
-                    <p class="card-text">Revisa y administra todos los datos de entrada y salida de los trabajadores desde un panel de control centralizado en tu PC.</p>
+            <div class="card_features">
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title about_us-card-title">Panel de Control</h5>
+                    <p class="card-text about_us-card-text">Revisa y administra todos los datos de entrada y salida de los trabajadores desde un panel de control centralizado en tu PC.</p>
                 </div>
             </div>
         </div>
         <div class="col-md-4 d-flex align-items-stretch">
-            <div class="card text-center shadow-sm">
-                <div class="card-body">
-                    <h5 class="card-title">Soporte 24/7</h5>
-                    <p class="card-text">Nuestro equipo de soporte está disponible las 24 horas del día, los 7 días de la semana, para ayudarte con cualquier problema o consulta.</p>
+            <div class="card_features">
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title about_us-card-title">Soporte 24/7</h5>
+                    <p class="card-text about_us-card-text">Nuestro equipo de soporte está disponible las 24 horas del día, los 7 días de la semana, para ayudarte con cualquier problema o consulta.</p>
                 </div>
             </div>
         </div>
