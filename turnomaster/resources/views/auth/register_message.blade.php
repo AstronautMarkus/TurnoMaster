ae0af946598247632ab68f347ac16cc6758e318c
Marcos Reyes <astronautmarkus@gmail.com>
2025-02-28 17:04:12 -0300

    feat: add registration success message and styling for user account creation

--- turnomaster/resources/views/auth/register_message.blade.php
+++ turnomaster/resources/views/auth/register_message.blade.php
@@ -1,10 +1,28 @@
 @extends('layouts.auth_app')
 
-@section('title', 'TurnoMaster - Verificación de Correo')
+<link rel="stylesheet" href="{{ asset('css/auth/register_message/register_message.css') }}">
+
+@section('title', 'TurnoMaster - ¡Cuenta Creada!')
 
 @section('content')
-<div class="verification-message-container">
-    <h2>{{ __('Verificación de Correo') }}</h2>
-    <p>{{ __('Hemos enviado un correo electrónico con un enlace de verificación. Por favor, revise su correo para activar su cuenta.') }}</p>
+<a href="{{ url('/') }}" class="home-button">
+    <i class="bi bi-house-door-fill"></i>{{ __('Inicio') }}
+</a>
+<div>
+    <div class="login-container">
+        <div class="login-header">
+            <div class="header-content">
+                <img src="{{ asset('img/logo/TurnoMaster_lines.svg') }}" alt="Login Image" class="login-image">
+                <h2>{{ __('¡Cuenta Creada!') }}</h2>
+            </div>
+            <div class="login-title">
+                <p>{{ __('¡Su cuenta ha sido creada con éxito! Por favor, revise su correo electrónico para activar su cuenta y comenzar a disfrutar de nuestra demo gratuita.') }}</p>
+                <p>{{ __('Si no ha recibido el correo, revise su carpeta de spam o haga clic en el botón de abajo para reenviar el correo de verificación.') }}</p>
+            </div>
+
+            <a class="btn-resend">Reenviar correo</a>
+            
+        </div>
+    </div>
 </div>
 @endsection
