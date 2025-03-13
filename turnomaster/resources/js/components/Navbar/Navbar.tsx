84886ddc4daa23117e0231d1a15b3d4cf9b28dae
Marcos Reyes <astronautmarkus@gmail.com>
2025-03-12 21:31:14 -0300

    refactor: simplify Navbar component by removing user name state and updating toggle icon

--- turnomaster/resources/js/components/Navbar/Navbar.tsx
+++ turnomaster/resources/js/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { FaAngleDown } from "react-icons/fa6";
+import React, { useState } from 'react';
+import { FaAngleDown, FaAngleUp } from "react-icons/fa6";
 import "./Navbar.css"
 
 interface NavbarProps {
@@ -8,14 +8,6 @@ interface NavbarProps {
 
 const Navbar: React.FC<NavbarProps> = ({ toggleRightSidebar }) => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-    const [userName, setUserName] = useState("");
-
-    useEffect(() => {
-        const user = JSON.parse(sessionStorage.getItem("user") || "{}");
-        if (user.name) {
-            setUserName(user.name.split(' ').map((word: string) => word.charAt(0).toUpperCase() + word.slice(1)).join(' '));
-        }
-    }, []);
 
     const handleToggle = () => {
         if (isSidebarOpen) {
@@ -31,11 +23,10 @@ const Navbar: React.FC<NavbarProps> = ({ toggleRightSidebar }) => {
         <nav className="navbar navbar-expand-lg navbar-light text-white w-100 navbar-custom">
             <div className="container-fluid">
                 <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-                    <span className="navbar-text text-white mx-2">
-                        ¡Hola, {userName}!
-                    </span>
-                    <div>
-                        <div className="toggle-dropdown" onClick={handleToggle}> <FaAngleDown /> </div>                 
+                    <div className='mx-3'>
+                        <div className="toggle-dropdown" onClick={handleToggle}>
+                            {isSidebarOpen ? <FaAngleUp /> : <FaAngleDown />}
+                        </div>                 
                     </div>
                 </div>
             </div>
