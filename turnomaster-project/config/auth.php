733b1ccd2a879d8cd1188fbec1f66b10d469605e
Marcos Reyes <astronautmarkus@gmail.com>
2025-03-29 16:29:20 -0300

    feat: created basic user and companies model

--- turnomaster-project/config/auth.php
+++ turnomaster-project/config/auth.php
@@ -14,9 +14,10 @@ return [
     */
 
     'defaults' => [
-        'guard' => env('AUTH_GUARD', 'web'),
+        'guard' => env('AUTH_GUARD', 'sanctum'),
         'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
     ],
+
 
     /*
     |--------------------------------------------------------------------------
