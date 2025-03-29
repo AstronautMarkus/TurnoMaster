e1730ab18b8b90503f1047d2975a4ee40ccde77c
Marcos Reyes <astronautmarkus@gmail.com>
2025-03-29 00:52:14 -0300

    feat: update NormalRouter to include additional routes within DefaultLayout

--- turnomaster-project/resources/js/routers/NormalRouter.tsx
+++ turnomaster-project/resources/js/routers/NormalRouter.tsx
@@ -5,9 +5,20 @@ import DefaultLayout from '../Layouts/Default/DefaultLayout';
 
 function NormalRouter() {
     return (
-        <Routes>
-            <Route path="/*" element={ <DefaultLayout> <IndexPage /> </DefaultLayout>}/>
-        </Routes>
+        <DefaultLayout>
+            <Routes>
+                <Route path="/*" element={<IndexPage />} />
+                <Route path="/prices" element={<h1>Prices Page</h1>} />
+                <Route path="/clients" element={<h1>Clients Page</h1>} />
+                <Route path="/about-project" element={<h1>About Project Page</h1>} />
+                <Route path="/features" element={<h1>Features Page</h1>} />
+                <Route path="/turnomaster-audience" element={<h1>TurnoMaster Audience Page</h1>} />
+                <Route path="/compare-plans" element={<h1>Compare Plans Page</h1>} />
+                <Route path="/integrations" element={<h1>Integrations Page</h1>} />
+                <Route path="/faq" element={<h1>FAQ Page</h1>} />
+                <Route path="/security-privacy" element={<h1>Security & Privacy Page</h1>} />
+            </Routes>
+        </DefaultLayout>
     );
 }
 
