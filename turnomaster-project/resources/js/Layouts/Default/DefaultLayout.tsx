36c85722e02deae7d6d41338c8158e434bc8d46f
Marcos Reyes <astronautmarkus@gmail.com>
2025-03-29 22:44:49 -0300

    feat: added basic footer structure

--- turnomaster-project/resources/js/Layouts/Default/DefaultLayout.tsx
+++ turnomaster-project/resources/js/Layouts/Default/DefaultLayout.tsx
@@ -1,5 +1,6 @@
 import type React from "react"
 import Navbar from "./Components/Navbar/Navbar"
+import Footer from "./Components/Footer/Footer"
 
 const DefaultLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
@@ -9,11 +10,7 @@ const DefaultLayout: React.FC<{ children: React.ReactNode }> = ({ children }) =>
       
       <main className="flex-grow flex">{children}</main>
 
-      <footer className="bg-gray-800 text-white p-4">
-        <div className="container mx-auto text-center">
-          <p>&copy; {new Date().getFullYear()} My App. All rights reserved.</p>
-        </div>
-      </footer>
+      <Footer />
     </div>
   )
 }
