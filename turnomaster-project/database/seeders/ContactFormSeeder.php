2f9b28b08f70886791d348245d37a2632761c668
Marcos Reyes <astronautmarkus@gmail.com>
2025-04-27 21:56:14 -0400

    fix: update namespace for ContactFormCategories in ContactFormSeeder

--- turnomaster-project/database/seeders/ContactFormSeeder.php
+++ turnomaster-project/database/seeders/ContactFormSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\ContactFormCategories;
+use App\Models\Forms\ContactFormCategories;
 
 class ContactFormSeeder extends Seeder
 {
