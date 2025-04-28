bf40eb75ddac2e45529252f4993ac23a9baab8ab
Marcos Reyes <astronautmarkus@gmail.com>
2025-04-27 21:33:17 -0400

    feat: create Subscriptions model, migration, and seeder

--- turnomaster-project/database/seeders/DatabaseSeeder.php
+++ turnomaster-project/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call(RolesTableSeeder::class);
         $this->call(ContactFormSeeder::class);
+        $this->call(SubscriptionsSeeder::class);
         
     }
 }
