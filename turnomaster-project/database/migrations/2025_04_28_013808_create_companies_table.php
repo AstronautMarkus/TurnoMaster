3256a3248f7d17513f22cb4989a7f8d34cac6aed
Marcos Reyes <astronautmarkus@gmail.com>
2025-04-27 21:39:26 -0400

    refactor: refactorized Companies model and migration with subscription relationship

--- turnomaster-project/database/migrations/2025_04_28_013808_create_companies_table.php
+++ turnomaster-project/database/migrations/2025_04_28_013808_create_companies_table.php
@@ -4,21 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCompaniesTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->unsignedBigInteger('owner_id')->nullable();
+            $table->unsignedBigInteger('subscription_id');
             $table->timestamps();
+
+            $table->foreign('subscription_id')->references('id')->on('subscriptions')->onDelete('cascade');
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::dropIfExists('companies');
     }
-}
-
+};
