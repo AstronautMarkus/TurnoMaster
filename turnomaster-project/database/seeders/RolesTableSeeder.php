63c7e684606e10fe94c18337ce42da7394142221
Marcos Reyes <astronautmarkus@gmail.com>
2025-04-27 21:24:43 -0400

    feat: add description field to roles and update seeder data

--- turnomaster-project/database/seeders/RolesTableSeeder.php
+++ turnomaster-project/database/seeders/RolesTableSeeder.php
@@ -12,10 +12,9 @@ class RolesTableSeeder extends Seeder
     {
 
         DB::table('roles')->insert([
-            ['name' => 'demo'],
-            ['name' => 'employee'],
-            ['name' => 'rh'],
-            ['name' => 'manager']        
+            ['name' => 'admin', 'description' => 'Administradores de la empresa, con acceso total a todas las funciones de la aplicación.'],
+            ['name' => 'hr', 'description' => 'Encargados de la gestión de recursos humanos, con acceso a funciones relacionadas con la gestión de personal.'],
+            ['name' => 'employee', 'description' => 'Empleados de las empresas, funciones básicas para el registro y reporte de horas, entradas/salidas y justificaciones especiales.']
         ]);
     }
 }
