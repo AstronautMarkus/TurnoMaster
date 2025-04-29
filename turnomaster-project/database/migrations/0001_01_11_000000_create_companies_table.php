f8627466ee6fa0b1825b4a9d0ce90083080d217b
Marcos Reyes <astronautmarkus@gmail.com>
2025-04-29 09:58:11 -0400

    refactor: refactorized models subscriptions and companies

--- turnomaster-project/database/migrations/0001_01_11_000000_create_companies_table.php
+++ turnomaster-project/database/migrations/0001_01_11_000000_create_companies_table.php
@@ -14,7 +14,9 @@ return new class extends Migration
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->unsignedBigInteger('subscription_id');
             $table->timestamps();
+            $table->foreign('subscription_id')->references('id')->on('subscriptions')->onDelete('cascade');
         });
     }
 
