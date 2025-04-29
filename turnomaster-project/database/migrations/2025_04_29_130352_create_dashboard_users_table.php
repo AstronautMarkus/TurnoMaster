e3be5922ca1cdb5b4833c2314428fe5911bdea10
Marcos Reyes <astronautmarkus@gmail.com>
2025-04-29 09:09:52 -0400

    feat: added dashboard users table and add foreign to roles and copmany.

---
+++ turnomaster-project/database/migrations/2025_04_29_130352_create_dashboard_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dashboard_users', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->unsignedBigInteger('rut');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->unsignedBigInteger('company_id');
+            $table->unsignedBigInteger('role_id');
+            $table->timestamps();
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dashboard_users');
+    }
+};
