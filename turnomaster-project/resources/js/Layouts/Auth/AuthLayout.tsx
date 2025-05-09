d692c1f37b61ed2f35a2e4e0c8a03b2b409af686
Marcos Reyes <astronautmarkus@gmail.com>
2025-05-09 01:51:58 -0400

    feat: add background image to AuthLayout component

--- turnomaster-project/resources/js/Layouts/Auth/AuthLayout.tsx
+++ turnomaster-project/resources/js/Layouts/Auth/AuthLayout.tsx
@@ -3,10 +3,11 @@ import type React from "react";
 const AuthLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className="relative w-full h-screen overflow-hidden bg-gray-500">
+      <div className="absolute inset-0 bg-cover bg-center bg-[url('/img/backgrounds/kate-sade-2zZp12ChxhU-unsplash.jpg')] brightness-50"></div>
       <div className="absolute inset-0 flex items-center justify-center">
-      <div className="flex flex-col items-center justify-center w-full px-4">
-        {children}
-      </div>
+        <div className="flex flex-col items-center justify-center w-full px-4">
+          {children}
+        </div>
       </div>
     </div>
   );
