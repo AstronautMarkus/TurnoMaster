9c75034ddfd1af4e348791ffbceefdf1119bf28b
Marcos Reyes <astronautmarkus@gmail.com>
2025-05-10 02:20:35 -0400

    fix: correct role names in RolesTableSeeder for consistency

--- turnomaster-project/database/seeders/RolesTableSeeder.php
+++ turnomaster-project/database/seeders/RolesTableSeeder.php
@@ -12,9 +12,9 @@ class RolesTableSeeder extends Seeder
     {
 
         DB::table('roles')->insert([
-            ['name' => 'admin', 'description' => 'Administradores de la empresa, con acceso total a todas las funciones de la aplicación.'],
-            ['name' => 'hr', 'description' => 'Encargados de la gestión de recursos humanos, con acceso a funciones relacionadas con la gestión de personal.'],
-            ['name' => 'employee', 'description' => 'Empleados de las empresas, funciones básicas para el registro y reporte de horas, entradas/salidas y justificaciones especiales.']
+            ['name' => 'Administrador', 'description' => 'Administradores de la empresa, con acceso total a todas las funciones de la aplicación.'],
+            ['name' => 'Recursos Humanos (RR. HH.)', 'description' => 'Encargados de la gestión de recursos humanos, con acceso a funciones relacionadas con la gestión de personal.'],
+            ['name' => 'Empleado', 'description' => 'Empleados de las empresas, funciones básicas para el registro y reporte de horas, entradas/salidas y justificaciones especiales.']
         ]);
     }
 }
