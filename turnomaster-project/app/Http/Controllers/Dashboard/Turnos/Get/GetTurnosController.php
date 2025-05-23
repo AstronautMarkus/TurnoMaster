96d3d41c7ca7691a74e61fd25de9ff05a2a5377b
Marcos Reyes <astronautmarkus@gmail.com>
2025-05-22 22:09:50 -0400

    feat: enhance GetTurnosController to pagination and search query params, and null validation in updated_at

--- turnomaster-project/app/Http/Controllers/Dashboard/Turnos/Get/GetTurnosController.php
+++ turnomaster-project/app/Http/Controllers/Dashboard/Turnos/Get/GetTurnosController.php
@@ -20,7 +20,23 @@ class GetTurnosController extends Controller
 
         $query = Turnos::where('company_id', $userCompany);
 
+        if ($request->has('name')) {
+            $name = $request->query('name');
+            $query->where(function ($q) use ($name) {
+            $q->where('name', 'like', '%' . $name . '%')
+              ->orWhere('description', 'like', '%' . $name . '%');
+            });
+        }
+
         $turnos = $query->paginate($perPage);
+
+        // if updated_at is equal to created_at, set updated_at to null
+        $turnos->getCollection()->transform(function ($item) {
+            if ($item->created_at == $item->updated_at) {
+                $item->updated_at = null;
+            }
+            return $item;
+        });
 
         return response()->json($turnos);
     }
