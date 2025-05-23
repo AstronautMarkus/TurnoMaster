a2121e9af1644c8b4bfe4e935acfe1b6a4513b62
Marcos Reyes <astronautmarkus@gmail.com>
2025-05-22 22:20:00 -0400

    feat: add UsersListTurno route,  view and hook

---
+++ turnomaster-project/resources/js/Pages/Dashboard/Turnos/Users/UsersListTurno.tsx
@@ -0,0 +1,10 @@
+const UsersListTurno = () => {
+    return (
+        <div className="p-6">
+            <h1 className="text-3xl sm:text-4xl font-bold text-left mb-6 mt-4 text-gray-800">Lista de usuarios</h1>
+
+
+        </div>
+    );
+};
+export default UsersListTurno;
--- turnomaster-project/resources/js/routers/Dashboard/Turnos/DashboardTurnosRouter.tsx
+++ turnomaster-project/resources/js/routers/Dashboard/Turnos/DashboardTurnosRouter.tsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import ListTurnos from '../../../Pages/Dashboard/Turnos/List/ListTurnos';
 import EditTurno from '../../../Pages/Dashboard/Turnos/Edit/EditTurno';
 import CreateTurno from '../../../Pages/Dashboard/Turnos/Create/CreateTurno';
+import UsersListTurno from '../../../Pages/Dashboard/Turnos/Users/UsersListTurno';
 
 function DashboardTurnosRouter() {
     return (
@@ -11,6 +12,7 @@ function DashboardTurnosRouter() {
             <Route path="/" element={<ListTurnos />} />
             <Route path="/create" element={<CreateTurno />} />
             <Route path="/edit/:id" element={<EditTurno />} />
+            <Route path="/:id/users" element={<UsersListTurno/>} />
         </Routes>
     );
 }
