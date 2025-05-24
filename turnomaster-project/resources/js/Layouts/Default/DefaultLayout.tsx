f3920c0b8add3e898f5f02839f417406b8f81959
Marcos Reyes <astronautmarkus@gmail.com>
2025-05-24 01:38:40 -0400

    fix: add background color to DefaultLayout for improved visibility

--- turnomaster-project/resources/js/Layouts/Default/DefaultLayout.tsx
+++ turnomaster-project/resources/js/Layouts/Default/DefaultLayout.tsx
@@ -4,7 +4,7 @@ import Footer from "./Components/Footer/Footer"
 
 const DefaultLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-gray-100">
 
       <Navbar />
       
