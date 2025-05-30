35883265eecea18e5f5d9a6aa6c9860bd01a0bc7
Marcos Reyes <astronautmarkus@gmail.com>
2025-05-30 05:25:48 -0400

    refactor: replace Navigate with window.location.href for unauthorized access redirection in ProtectedRoute

--- turnomaster-project/resources/js/Layouts/Dashboard/Components/ProtectedRoute/ProtectedRoute.tsx
+++ turnomaster-project/resources/js/Layouts/Dashboard/Components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
 import useRoleChecker from '../../../../hooks/auth/useRoleChecker';
 
 interface ProtectedRouteProps {
@@ -9,14 +8,14 @@ interface ProtectedRouteProps {
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children, allowedRoles }) => {
     const userRole = useRoleChecker();
-    const location = useLocation();
 
     if (userRole === null) {
         return null;
     }
 
     if (!allowedRoles.includes(userRole)) {
-        return <Navigate to={`/dashboard?unauthorized`} state={{ from: location }} replace />;
+        window.location.href = `/dashboard?unauthorized`;
+        return null;
     }
 
     return <>{children}</>;
