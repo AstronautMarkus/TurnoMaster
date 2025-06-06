d28bbda664c28564e6233db2186712184d808df3
Marcos Reyes <astronautmarkus@gmail.com>
2025-06-06 01:42:52 -0400

    refactor: added JWT data get and validations for auto deleting, differentiating the user 'company' from 'employee' and deleting another company users.

--- turnomaster-project/app/Http/Controllers/Dashboard/Employees/Delete/DeleteEmployeeController.php
+++ turnomaster-project/app/Http/Controllers/Dashboard/Employees/Delete/DeleteEmployeeController.php
@@ -5,11 +5,20 @@ namespace App\Http\Controllers\Dashboard\Employees\Delete;
 use App\Http\Controllers\Controller;
 use App\Models\Users\DashboardUser;
 use Illuminate\Http\Request;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 class DeleteEmployeeController extends Controller
 {
-    public function deleteEmployee($id)
+    public function deleteEmployee(Request $request, $id)
     {
+
+        $token = $request->bearerToken();
+        $decoded = JWT::decode($token, new Key(env('JWT_SECRET'), 'HS256'));
+        $userType = $decoded->user_type;
+        $userId = $decoded->user_id;
+        $companyId = $decoded->company_id;
+
         $user = DashboardUser::find($id);
 
         if (!$user) {
@@ -17,6 +26,22 @@ class DeleteEmployeeController extends Controller
                 'message' => 'El empleado no existe.',
             ], 404);
         }
+
+        if ($userType === 'employee') {
+            
+            if ($userId == $id) {
+                return response()->json([
+                    'message' => 'No puedes eliminarte a ti mismo.',
+                ], 403);
+            }
+            
+            if ($user->company_id != $companyId) {
+                return response()->json([
+                    'message' => 'No tienes permiso para eliminar empleados de otra empresa.',
+                ], 403);
+            }
+        }
+        
 
         $user->delete();
 
