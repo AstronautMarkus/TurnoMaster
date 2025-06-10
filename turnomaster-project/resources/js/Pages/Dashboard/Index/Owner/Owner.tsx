2b3cd735204a42c611c554d904e058fd64904a27
Marcos Reyes <astronautmarkus@gmail.com>
2025-06-10 00:18:55 -0400

    refactor: Owner section became responsive and added 'Registro de actividades' Button.

--- turnomaster-project/resources/js/Pages/Dashboard/Index/Owner/Owner.tsx
+++ turnomaster-project/resources/js/Pages/Dashboard/Index/Owner/Owner.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect, JSX } from 'react';
 import { FaCalendar, FaUsers } from 'react-icons/fa6';
 import { Link } from 'react-router-dom';
 import { FaUserShield, FaUser, FaBuilding } from 'react-icons/fa';
-import { FaUserGear } from "react-icons/fa6";
+import { FaUserGear, FaBook } from "react-icons/fa6";
 import { FiSettings } from 'react-icons/fi';
 import useGetCompanyDetails from './useGetCompanyDetails';
 
@@ -61,33 +61,54 @@ const Owner = () => {
         <div className="p-6">
             <h1 className="text-3xl sm:text-4xl font-bold text-left mb-6 mt-4">TurnoMaster - Inicio</h1>
             
-            <div className="flex flex-col lg:flex-row gap-4">
-                <div className="flex-grow bg-white shadow-md sm:p-6">
-                    <div className="mb-6">
-                        <h2 className="text-xl sm:text-2xl font-semibold mb-2">{greeting}.</h2>
-                        <p className="text-sm sm:text-base">Este es el panel de control de TurnoMaster, donde podrás gestionar tus turnos y citas de manera eficiente.</p>
+            <div className="flex flex-col md:flex-row gap-4 items-center">
+                <div className="flex-grow bg-white shadow-md sm:p-6 w-full">
+                    <div className="mb-6 text-center">
+                        <h2 className="text-xl sm:text-2xl font-semibold mb-2 mt-4">{greeting}.</h2>
+                        <p className="text-sm sm:text-base">Este es el panel central de TurnoMaster, orientado al dueño/a de la empresa.</p>
                     </div>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                        <Link to="/dashboard/turnos" className="dashboard-button text-white p-4 rounded-none flex flex-col items-center justify-center w-full sm:w-10/12 mx-auto sm:ml-0 sm:mr-auto transition-colors">
-                            <div className="flex items-center">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 justify-items-center">
+                        <Link
+                            to="/dashboard/turnos"
+                            className="dashboard-button text-white p-4 rounded-none flex flex-col items-center justify-center w-[90%] transition-colors"
+                        >
+                            <div className="flex items-center justify-center">
                                 <div className="text-4xl mr-4">
                                     <FaCalendar />
                                 </div>
-                                <div>
+                                <div className="text-center">
                                     <h3 className="text-xl font-bold">Turnos</h3>
-                                    <p className="text-sm">Gestiona tus turnos aquí</p>
+                                    <p className="text-sm">Gestionar Turnos</p>
                                 </div>
                             </div>
                         </Link>
-                        
-                        <Link to="/dashboard/employees" className="dashboard-button-secondary text-white p-4 rounded-none flex flex-col items-center justify-center w-full sm:w-10/12 mx-auto sm:ml-0 sm:mr-auto transition-colors">
-                            <div className="flex items-center">
+
+                        <Link
+                            to="/dashboard/employees"
+                            className="dashboard-button-secondary text-white p-4 rounded-none flex flex-col items-center justify-center w-[90%] transition-colors"
+                        >
+                            <div className="flex items-center justify-center">
                                 <div className="text-4xl mr-4">
                                     <FaUsers />
                                 </div>
-                                <div>
+                                <div className="text-center">
                                     <h3 className="text-xl font-bold">Empleados</h3>
-                                    <p className="text-sm">Gestiona tus empleados aquí</p>
+                                    <p className="text-sm">Gestionar empleados</p>
+                                </div>
+                            </div>
+                        </Link>
+
+                        <Link
+                            to="/dashboard/logs/activity"
+                            className="dashboard-button-success text-white p-4 rounded-none flex flex-col items-center justify-center w-[90%] transition-colors mb-4"
+                        >
+                            <div className="flex items-center justify-center">
+                                <div className="text-4xl mr-4">
+                                    <FaBook />
+                                </div>
+                                <div className="text-center">
+                                    <h3 className="text-xl font-bold">Registro de Actividades</h3>
+                                    <p className="text-sm">Monitorea todos los movimientos de la empresa</p>
                                 </div>
                             </div>
                         </Link>
