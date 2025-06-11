2a937aab7399e417c7052d06536de5df3788a638
Marcos Reyes <astronautmarkus@gmail.com>
2025-06-10 22:57:25 -0400

    refactor: enhance image upload functionality with drag-and-drop support and improved styling

--- turnomaster-project/resources/js/Pages/Dashboard/Profile/Profile.tsx
+++ turnomaster-project/resources/js/Pages/Dashboard/Profile/Profile.tsx
@@ -11,6 +11,7 @@ const Profile: React.FC = () => {
     const { updateImage, deleteImage, isLoading, error, isSuccess } = useUpdateImage();
     const [previewImage, setPreviewImage] = useState<string | null>(null);
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isDragOver, setIsDragOver] = useState(false);
 
     const handleImageChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
         if (event.target.files && event.target.files[0]) {
@@ -25,18 +26,31 @@ const Profile: React.FC = () => {
         }
     };
 
+    const handleDrop = async (event: React.DragEvent<HTMLDivElement>) => {
+        event.preventDefault();
+        setIsDragOver(false);
+        if (event.dataTransfer.files && event.dataTransfer.files[0]) {
+            const file = event.dataTransfer.files[0];
+            setPreviewImage(URL.createObjectURL(file));
+            try {
+                await updateImage(file);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    };
+
     return (
         <div className="p-6">
             <h1 className="text-4xl font-bold text-left mb-6 mt-4">Mi perfil</h1>
-            
-            <div className="flex gap-4">
-                {!user ? (
-                    <div className="flex items-center justify-center w-full">
-                        <LoadingScreen theme='dashboard' />
-                    </div>
-                ) : (
-                    <>
-                        <div className="flex-grow w-9/10 bg-white shadow-md sm:p-6">
+            {!user ? (
+                <div className="flex items-center justify-center w-full">
+                    <LoadingScreen theme='dashboard' />
+                </div>
+            ) : (
+                <>
+                    <div className="flex flex-col lg:flex-row gap-6">
+                        <div className="bg-white shadow-md sm:p-6 flex-1 mb-4 lg:mb-0">
                             <div className="mb-6">
                                 <h2 className="text-2xl font-semibold mb-2">Información Personal</h2>
                                 <p>Aquí puedes ver los detalles de tu perfil personal.</p>
@@ -68,10 +82,9 @@ const Profile: React.FC = () => {
                                 </div>
                             </div>
                         </div>
-
-                        <div className="w-1/10 bg-white shadow-md sm:p-6 flex flex-col items-center">
+                        <div className="bg-white shadow-md sm:p-6 flex flex-col items-center w-full lg:w-80">
                             <h2 className="text-1xl font-semibold mb-2">Imagen de perfil</h2> 
-                            <div className="w-48 h-48 bg-gray-200 overflow-hidden rounded">
+                            <div className="w-48 h-48 bg-gray-200 overflow-hidden border-2 border-dashed border-gray-400">
                                 <img 
                                     src={user.profile_photo} 
                                     className="object-cover w-full h-full" 
@@ -85,84 +98,102 @@ const Profile: React.FC = () => {
                                 Cambiar foto
                             </button>
                         </div>
-                    </>
-                )}
-            </div>
-
-            {isModalOpen && (
-                <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-                    <div className="bg-white p-6 rounded shadow-lg w-96">
-                        <h2 className="text-xl font-bold mb-4">Cambiar foto de perfil</h2>
-                        <div className="w-48 h-48 bg-gray-200 overflow-hidden rounded mx-auto mb-4">
-                            <img 
-                                src={previewImage || user?.profile_photo} 
-                                className="object-cover w-full h-full" 
-                                alt="Preview"
-                            />
-                        </div>
-                        <input 
-                            type="file" 
-                            accept="image/*" 
-                            className="block w-full mb-4"
-                            onChange={handleImageChange} 
-                            disabled={isLoading}
-                        />
-                        {isLoading && <p className="text-sm dashboard-text mb-2">Subiendo imagen...</p>}
-                        {error && <strong className="text-sm dashboard-text-error mb-2">{error}</strong>}
-                        {isSuccess && <p className="text-m dashboard-text-success mb-2">Imagen actualizada correctamente.</p>}
-                        <div className="flex justify-end gap-2 mt-4">
-                            <button 
-                                className="text-white px-4 py-2 dashboard-button transition-colors"
-                                onClick={async () => {
-                                    try {
-                                        await deleteImage();
-                                        setPreviewImage(null);
-                                        setIsModalOpen(false);
-                                    } catch (err) {
-                                        console.error(err);
-                                    }
-                                }}
-                                disabled={isLoading}
-                            >
-                                Borrar imagen
-                            </button>
-                            <button 
-                                className="px-4 py-2 text-white dashboard-button-secondary transition-colors"
-                                onClick={() => setIsModalOpen(false)}
-                            >
-                                Cerrar
-                            </button>
-                        </div>
                     </div>
-                </div>
-            )}
 
-            {user && (
-                <div className="mt-6 bg-white shadow-md sm:p-6">
-                    <h2 className="text-2xl font-semibold mb-4">Rol empleado en {user.company?.name}</h2>
-                    <div className="flex items-center gap-4">
-                        <div className="text-4xl">
-                            {user.role.id === 1 ? <FaUserShield /> : user.role.id === 3 ? <FaUser /> : <FaUserGear />}
-                        </div>
-                        <div>
-                            <p className="text-lg font-bold">{user.role.name}</p>
-                            <p className="italic">{user.role.description}</p>
-                        </div>
-                    </div>
-                    {user.email === user.company?.owner_email && (
-                        <div className="mt-6 p-4 dashboard-background-secondary flex items-center gap-4">
-                            <div className="text-4xl text-white">
-                                <FaCrown/>
-                            </div>
-                            <div>
-                                <h3 className="text-xl font-semibold mb-1 flex items-center text-white">Dueño de la compañía</h3>
-                                <p className="text-white italic">Este usuario es el dueño de la compañía <strong>{user.company?.name}</strong>.</p>
+                    {isModalOpen && (
+                        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+                            <div className="bg-white p-6 shadow-lg w-96">
+                                <h2 className="text-xl font-bold mb-4">Cambiar foto de perfil</h2>
+                                <div className="w-48 h-48 bg-gray-200 overflow-hidden mx-auto mb-4 border-2 border-dashed border-gray-400">
+                                    <img 
+                                        src={previewImage || user?.profile_photo} 
+                                        className="object-cover w-full h-full" 
+                                        alt="Preview"
+                                    />
+                                </div>
+                                <div
+                                    className={`dashboard-background-secondary p-4 mb-4 flex flex-col items-center border-2 transition-colors ${
+                                        isDragOver ? 'border-blue-400 bg-blue-100' : 'border-gray-400'
+                                    }`}
+                                    onDragOver={e => { e.preventDefault(); setIsDragOver(true); }}
+                                    onDragLeave={e => { e.preventDefault(); setIsDragOver(false); }}
+                                    onDrop={handleDrop}
+                                >
+                                    <label 
+                                        htmlFor="profile-image-upload" 
+                                        className="cursor-pointer text-white font-semibold px-4 py-2 dashboard-button-success mb-2 transition-colors"
+                                    >
+                                        Seleccionar imagen
+                                    </label>
+                                    <input 
+                                        id="profile-image-upload"
+                                        type="file" 
+                                        accept="image/*" 
+                                        className="hidden"
+                                        onChange={handleImageChange} 
+                                        disabled={isLoading}
+                                    />
+                                    <span className="text-white text-sm">
+                                        Formatos permitidos: JPG, PNG, GIF<br/>
+                                        o arrastra la imagen aquí
+                                    </span>
+                                </div>
+                                {isLoading && <p className="text-sm dashboard-text mb-2">Subiendo imagen...</p>}
+                                {error && <strong className="text-sm dashboard-text-error mb-2">{error}</strong>}
+                                {isSuccess && <p className="text-m dashboard-text-success mb-2">Imagen actualizada correctamente.</p>}
+                                <div className="flex justify-end gap-2 mt-4">
+                                    <button 
+                                        className="text-white px-4 py-2 dashboard-button transition-colors"
+                                        onClick={async () => {
+                                            try {
+                                                await deleteImage();
+                                                setPreviewImage(null);
+                                                setIsModalOpen(false);
+                                            } catch (err) {
+                                                console.error(err);
+                                            }
+                                        }}
+                                        disabled={isLoading}
+                                    >
+                                        Borrar imagen
+                                    </button>
+                                    <button 
+                                        className="px-4 py-2 text-white dashboard-button-secondary transition-colors"
+                                        onClick={() => setIsModalOpen(false)}
+                                    >
+                                        Cerrar
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     )}
-                </div>
+
+                    {/* Rol y dueño */}
+                    <div className="mt-6 bg-white shadow-md sm:p-6">
+                        <h2 className="text-2xl font-semibold mb-4">Rol empleado en {user.company?.name}</h2>
+                        <div className="flex items-center gap-4">
+                            <div className="text-4xl">
+                                {user.role.id === 1 ? <FaUserShield /> : user.role.id === 3 ? <FaUser /> : <FaUserGear />}
+                            </div>
+                            <div>
+                                <p className="text-lg font-bold">{user.role.name}</p>
+                                <p className="italic">{user.role.description}</p>
+                            </div>
+                        </div>
+                        {user.email === user.company?.owner_email && (
+                            <div className="mt-6 p-4 dashboard-background-secondary flex items-center gap-4">
+                                <div className="text-4xl text-white">
+                                    <FaCrown/>
+                                </div>
+                                <div>
+                                    <h3 className="text-xl font-semibold mb-1 flex items-center text-white">Dueño de la compañía</h3>
+                                    <p className="text-white italic">Este usuario es el dueño de la compañía <strong>{user.company?.name}</strong>.</p>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </>
             )}
-            
             <div className="flex space-x-2 justify-end mt-4">
                 <Link to="/dashboard/settings" className="text-white px-4 py-2 dashboard-button transition-colors">Salir</Link>
             </div>
